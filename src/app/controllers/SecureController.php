7c5bcb0fad7d0b6468743011050cce8ff5d2800b
Utkarsh_patel <utkarshpatel@cedcoss.com>
2022-04-01 12:10:58 +0530

    Improved ACL implementation

--- src/app/controllers/SecureController.php
+++ src/app/controllers/SecureController.php
@@ -33,7 +33,8 @@ class SecureController extends Controller
             );
 
             if (count($data) > 0) {
-
+                // print_r($data);
+                // die();
                 $comp = Components :: find();
 
                 foreach($data as $key=>$val)
@@ -42,27 +43,41 @@ class SecureController extends Controller
 
                     if ($key == 'role') {
                         $acl->addRole($val);
-                    } else {
                         foreach($comp as $key1=>$val1)
                         {
-                            // echo $val1->action."<br>";
                             $acl->addComponent(
                                 $val1->controller,
                                 [
                                     $val1->action
                                 ]
                             );
+                            $acl->deny($data['role'], $val1->controller, $val1->action);
+                        }
+                    } else {
+                        foreach($comp as $key1=>$val1)
+                        {
+                            // echo $val1->action."<br>";
+                            // $acl->addComponent(
+                            //     $val1->controller,
+                            //     [
+                            //         $val1->action
+                            //     ]
+                            // );
+                            // echo $val1->controller."=".$val1->action."=".$val1->id."<br>";
+                            // echo  $val ."<br>";
                             if ($val1->id == $val) {
-
+                                
                                 $acl->allow($data['role'], $val1->controller, $val1->action);
-
-                            } else {
-                                $acl->deny($data['role'], $val1->controller, $val1->action);
-                            }
+                                
+                            } 
+                            // else {
+                            //     $acl->deny($data['role'], $val1->controller, $val1->action);
+                            // }
                         }
                     }
-
+                    
                 }
+                //die;
                 file_put_contents(
                     $aclfile,
                     serialize($acl)
